8dbd5b9979c3c2bbc7e4215fa7138208a5e71d66
malaramofficial <malaramofficial@gmail.com>
2026-09-04 22:56:06 +0530

    Finalize Android app build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,24 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-android { namespace = "com.rlp.postermaker"; compileSdk = 35
-    defaultConfig { applicationId = "com.rlp.postermaker"; minSdk = 24; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+android {
+    namespace = "com.rlp.postermaker"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.rlp.postermaker"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
 }
 
 dependencies {
